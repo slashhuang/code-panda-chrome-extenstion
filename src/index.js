8e269b5ebf9d03cff27f14af723d2816b6e5feb0
slashhuang <627284482@qq.com>
2016-09-11 16:41:57 +0800

    adding bael

---
+++ src/index.js
@@ -0,0 +1,58 @@
+/*
+* @Author slashhunag
+* 主页面
+*/
+    let _react = {
+      _name:'ReactJS',
+      sub:[{
+        name:'api',
+        url:'https://facebook.github.io/react/docs/component-api.html'
+      },{
+        name:'redux',
+        url:'https://github.com/reactjs/redux'
+      },
+      {
+        name:'react-native',
+        url:'https://facebook.github.io/react-native/'
+      }
+      ]};
+    let _vueJs = {
+      _name:'VueJS',
+      sub:[{
+        name:'api',
+        url:'http://vuejs.org/api/'
+      },{
+        name:'vue-router',
+        url:'http://router.vuejs.org/zh-cn/'
+      }]
+    };
+    let _webpack =  {
+      _name:'webpack',
+      sub:[{
+        name:'api',
+        url:'https://webpack.github.io/docs/configuration.html'
+      },{
+        name:'plugins',
+        url:'https://webpack.github.io/docs/list-of-plugins.html'
+      }]};
+    let _ng = {
+      _name:'Angular',
+      sub:[
+      {
+        name:'ng1',
+        url:'https://docs.angularjs.org/api'
+      },{
+        name:'ng2',
+        url:'https://angular.io/docs/ts/latest/api/'
+      }]
+    };
+    let _const = [_react,_vueJs,_webpack,_ng]; 
+    module.exports = _const;
+
+
+
+
+
+
+
+     
